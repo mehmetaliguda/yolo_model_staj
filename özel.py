6b83c33e6f70d1e5391fc8f883ea129548e87161
mehmetaliguda <mehmetaliguda91@gmail.com>
2026-07-23 14:04:13 +0300

    bir dosya daha silindi

--- özel.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv("./sample_submission.csv")   # val icin kullandiginiz csv'nin gercek yolu
-print(df["ClassId"].value_counts())
-print("toplam benzersiz goruntu:", df["ImageId"].nunique())
